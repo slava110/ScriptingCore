5aa8d577bee56b1122d11f64f8256c26342458b8
slava_110 <slava.hero@bk.ru>
2019-01-22 04:36:01 +0300

    Delete Install.js

--- Install.js
+++
@@ -1,9 +0,0 @@
-var InstallPath = new java.io.File(API.getWorldDir().getAbsolutePath() + "\\scripts\\ecmascript\\Core.js");
-var ScriptController = Java.type("noppes.npcs.controllers.ScriptController").Instance;
-
-function init(event){
-  event.player.message("Процесс установки Scripting Core запущен. Пожалуйста, подождите...");
-  org.apache.commons.io.FileUtils.copyURLToFile(new java.net.URL("https://raw.githubusercontent.com/slava110/ScriptingCore/master/Scripts/Test.js"), InstallPath);
-  ScriptController.playerScripts.getScripts()[0].scripts = ["Core.js"];
-  event.player.message("Вроде готово. Maybe ready");
-}
