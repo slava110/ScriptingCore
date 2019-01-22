57be148856c571f355450d01a250a10561181c44
slava_110 <slava.hero@bk.ru>
2019-01-22 04:35:43 +0300

    Update Install.js

--- Install.js
+++ Install.js
@@ -4,6 +4,6 @@ var ScriptController = Java.type("noppes.npcs.controllers.ScriptController").Ins
 function init(event){
   event.player.message("Процесс установки Scripting Core запущен. Пожалуйста, подождите...");
   org.apache.commons.io.FileUtils.copyURLToFile(new java.net.URL("https://raw.githubusercontent.com/slava110/ScriptingCore/master/Scripts/Test.js"), InstallPath);
-  ScriptController.playerScripts.getScripts()[0].scripts = ["Test.js"];
+  ScriptController.playerScripts.getScripts()[0].scripts = ["Core.js"];
   event.player.message("Вроде готово. Maybe ready");
 }
